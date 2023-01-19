33f0bb62f2afbae8d2bb69c444ca811011e6312f
abhi-ghute <abghute7@gmail.com>
2023-01-19 19:26:01 +0530

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignINComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignINComponent
+    SignINComponent,
+    SignUpComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
